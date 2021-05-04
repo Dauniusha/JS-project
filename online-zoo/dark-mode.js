54207301eb2913617250441b76298d04b6982189
Dauniusha <78425928+Dauniusha@users.noreply.github.com>
2021-05-04 04:12:08 +0300

    add: full-time dark mode

--- online-zoo/dark-mode.js
+++ online-zoo/dark-mode.js
@@ -12,14 +12,21 @@ function toggleTheme() {
     elems.forEach(elem => {
       elem.setAttribute('data-theme', 'dark');
     });
+    sessionStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     elems.forEach(elem => {
       elem.setAttribute('data-theme', 'light');
     });
+    sessionStorage.setItem('theme', 'light');
   }
 }
 
 let checkbox = document.getElementById('switch');
 const elems = document.querySelectorAll('div[data-theme]');
 checkbox.addEventListener('change', toggleTheme);
+
+if(sessionStorage.getItem('theme') === 'dark') {
+  checkbox.click();
+  toggleTheme.call(checkbox);
+}
